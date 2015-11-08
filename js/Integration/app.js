d11cbe07731e3bf7cd2bb68c59617275563b214e
jdolores <josemaria.dolores@gmail.com>
2015-11-07 21:02:50 -0500

    Added MedicationDispense Create and Delete. Like the other objects, Update does not work. Also Create does not persist fields: Quantity and DaysSupply.

--- js/Integration/app.js
+++ js/Integration/app.js
@@ -11,6 +11,7 @@ var MedicationPrescription = require("./MedicationPrescription");
 var ValueQuantity = require("./ValueQuantity");
 var Coding = require("./Coding");
 var Text = require("./Text");
+var Quantity = require("./Quantity");
 
 app.get('/', function(request, response) {
 	var fhir = new Fhir();
@@ -21,7 +22,7 @@ app.get('/', function(request, response) {
 	//fhir.getResourceById("Observation", "40857", function(resp) { response.send(resp.jsonPrint());});
 	//fhir.getResourceById("Condition", "3187", function(resp) { response.send(resp.jsonPrint());});
 	//fhir.getResourceById("Medication", "528264", function(resp) { response.send(resp.jsonPrint());});
-	//fhir.getResourceById("MedicationDispense", "765", function(resp) { response.send(resp.jsonPrint());});
+	//fhir.getResourceById("MedicationDispense", "774", function(resp) { response.send(resp.jsonPrint());});
 	//fhir.getResourceById("MedicationPrescription", "2048", function(resp) { response.send(resp.jsonPrint());});
 
 	//DEBUG METHODS
@@ -30,7 +31,7 @@ app.get('/', function(request, response) {
 	//fhir.printResourceById("Observation", "40857", function(resp) { response.send(resp);});
 	//fhir.printResourceById("Condition", "3187", function(resp) { response.send(resp);});
 	//fhir.printResourceById("Medication", "528264", function(resp) { response.send(resp);});
-	//fhir.printResourceById("MedicationDispense", "765", function(resp) { response.send(resp);});
+	//fhir.printResourceById("MedicationDispense", "774", function(resp) { response.send(resp);});
 	//fhir.printResourceById("MedicationPrescription", "2048", function(resp) { response.send(resp);});
 
 	//CREATE
@@ -41,7 +42,9 @@ app.get('/', function(request, response) {
 	//var entity = new Observation("Observation", "",	new Coding("http://loinc.org", "3141-9", "Body weight Measured"),	new ValueQuantity(77.1, "kg", "http://unitsofmeasure.org", "kg"),	"2015-10-26T20:12:00-04:00", "final", "ok", "556", "7346");
 	//var entity = new Condition("Condition", "", "556", "7346", "5", new Coding("http://snomed.info/sct", "230265002", "Familial Alzheimer's disease of early onset"),	"Familial Alzheimer's disease of early onset, SNOMED-CT, 230265002", "confirmed", "2002-05-26T00:00:00-04:00");
 	//There is no Create or Update Available for Medication object.
-	
+
+	//Create for Medication Dispense does not persist the quantity or the days supply
+	//var entity = new MedicationDispense("MedicationDispense", "", "556", new Quantity(30, "mg"), 30, "19080129", "2005-07-24T18:16:00-04:00");
 	//fhir.insertOrUpdateResource(entity, function(resp) { response.send(resp);});
 
 	//UPDATE
@@ -61,6 +64,8 @@ app.get('/', function(request, response) {
 
 	//There is no Create or Update Available for Medication object.
 
+	//There appears to be a bug with Observation Update in the FHIR Server so this doesn't work.
+	//var entity = new MedicationDispense("MedicationDispense", "2891", "556", new Quantity(30, "mg"), 15, "19080129", "2005-07-24T18:16:00-04:00");
 	//fhir.insertOrUpdateResource(entity, function(resp) { response.send(resp);});
 
 	//DELETE
@@ -71,6 +76,7 @@ app.get('/', function(request, response) {
 	//var entity = new Observation("Observation", "41188",	new Coding("http://loinc.org", "3141-9", "Body weight Measured"),	new ValueQuantity(77.1, "kg", "http://unitsofmeasure.org", "kg"),	"2015-10-26T20:12:00-04:00", "final", "ok", "556", "7346");
 	//var entity = new Condition("Condition", "3190", "556", "7346", "5", new Coding("http://snomed.info/sct", "230265002", "Familial Alzheimer's disease of early onset"),	"Familial Alzheimer's disease of early onset, SNOMED-CT, 230265002", "confirmed", "2002-05-26T00:00:00-04:00");
 	//var entity = new Medication("Medication", "528264", new Text("empty", "<div>No narrative available - Error: Error during execution of processor 'ca.uhn.fhir.narrative.BaseThymeleafNarrativeGenerator$NarrativeAttributeProcessor' (medication:4)</div>"), "Immune Globulin (Human) 180 UNT/ML", new Coding("http://www.nlm.nih.gov/research/umls/rxnorm", "797551", "edu.gatech.i3l.fhir.dstu2.entities.MedicationConcept@70890f1b"));
+	//var entity = new MedicationDispense("MedicationDispense", "2889", "556", new Quantity(30, "mg"), 15, "19080129", "2005-07-24T18:16:00-04:00");
 	//fhir.deleteResource(entity, function(resp) { response.send(resp);});
 
 });
