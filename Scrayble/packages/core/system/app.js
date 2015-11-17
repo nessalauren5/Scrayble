f72d309f85e6e8462e49bbbe9278eea2cd56d885
Vanessa Anderson <Vanessa.Anderson@teachforamerica.org>
2015-11-17 18:14:17 -0500

    add css, js, img theme dependencies

--- Scrayble/packages/core/system/app.js
+++ Scrayble/packages/core/system/app.js
@@ -22,9 +22,22 @@ SystemPackage.register(function(app, auth, database, circles) {
   SystemPackage.aggregateAsset('css', 'common.css');
 
   var libDir = "../lib/";
-  SystemPackage.aggregateAssets('css',libDir + "freelancer.css");
-  SystemPackage.aggregateAssets('css',libDir + "styles.css");
-  SystemPackage.aggregateAssets('css',libDir + "switchery.min.css");
+  SystemPackage.aggregateAssets('css',libDir + "css/freelancer.css");
+  SystemPackage.aggregateAssets('css',libDir + "css/styles.css");
+  SystemPackage.aggregateAssets('css',libDir + "css/switchery.min.css");
+
+
+  SystemPackage.aggregateAssets('js',libDir + "js/bootstrap.min.js");
+  SystemPackage.aggregateAssets('js',libDir + "js/js/cbpAnimatedHeader.min.js");
+  SystemPackage.aggregateAssets('js',libDir + "js/classie.js");
+  SystemPackage.aggregateAssets('js',libDir + "js/contact_me.js");
+  SystemPackage.aggregateAssets('js',libDir + "js/jqBootstrapValidation.js");
+  SystemPackage.aggregateAssets('js',libDir + "js/jquery.js");
+  SystemPackage.aggregateAssets('js',libDir + "js/jquery.validate.js");
+  SystemPackage.aggregateAssets('js',libDir + "js/switchery.min.js");
+
+
+  SystemPackage.aggregateAssets('img',libDir + "img/profile.png");
   SystemPackage.angularDependencies(['mean-factory-interceptor']);
 
 
